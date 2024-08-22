c44a6b2335a9549ec5957bc57b61898124586bc8
amutsamoses <amutsamoses@gmail.com>
2024-08-22 13:48:29 -0700

    Spies

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,30 @@
+/**
+ * utility module
+ */
+const Utils = {
+  /**
+ * Adds, subtracts or divides two numbers depending
+ * on operator type specified
+ * @param   {string}  type  - SUM, SUBTRACT, or DIVIDE
+ * @param   {number}  a
+ * @param   {number}  b
+ * @returns {number}  -  The result of operation of a and b
+ */
+  calculateNumber(type, a, b) {
+    const num1 = Math.round(a);
+    const num2 = Math.round(b);
+    switch (type) {
+      case 'SUM':
+        return num1 + num2;
+      case 'SUBTRACT':
+        return num1 - num2;
+      case 'DIVIDE':
+        if (!num2) return 'Error';
+        return num1 / num2;
+      default:
+        throw new Error('Operation not supported. Type must be SUM, SUBTRACT or DIVIDE');
+    }
+  },
+};
+
+module.exports = Utils;
