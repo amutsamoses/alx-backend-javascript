7c614c3951646a9b574d669e5be82b6737634545
amutsamoses <amutsamoses@gmail.com>
2024-09-17 19:12:26 +0000

    basic Nodejs

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * prints the message on the console
+ * @param{message} string - message to be printed
+ *
+ */
+
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
